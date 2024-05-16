0117953ad7c0b5bc53979209fa8b205af6b7c84f
Aamir21344011 <21344011@stu.mmu.ac.uk>
2024-05-16 23:51:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPopup(id) {
+    var popup = document.getElementById("popup" + id);
+    popup.style.display = "block";
+}
+
+function closePopup(id) {
+    var popup = document.getElementById("popup" + id);
+    popup.style.display = "none";
+}
